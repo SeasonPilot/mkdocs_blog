839c59dbf65ddc121a399d5edaf694fbfabe14d7
gavin <zhishengqianjun@163.com>
2019-08-23 08:41:39 +0800

    mod baidu tongji

--- docs/js/baidu-tongji.js
+++ docs/js/baidu-tongji.js
@@ -1,7 +1,9 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?03806bd68e029f12f863ba16efac2c2c";
+  hm.src = "https://hm.baidu.com/hm.js?a1c96c23ca24491f47f6825174f9b530";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
