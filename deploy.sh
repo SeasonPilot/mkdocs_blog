3985addc4b242f1e244042585174f86ef439748f
gavin <zhishengqianjun@163.com>
2019-11-27 14:19:05 +0800

    mod deploy.sh, .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf /usr/share/nginx/html_back  && git clone --depth=1 https://github.com/gaviners/gaviners.github.io  /usr/share/nginx/html_back
+rm -rf /usr/share/nginx/html_back  && git clone https://git.dev.tencent.com/zhishengqianjun/blog.git  /usr/share/nginx/html_back
 
 rm -rf /usr/share/nginx/html && mv /usr/share/nginx/html_back /usr/share/nginx/html
