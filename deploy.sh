520a0bb191fc90dd1a60dece4c56ca769f771fd3
gavin <zhishengqianjun@163.com>
2019-12-17 20:41:58 +0800

    mod deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@
 rm -rf /usr/share/nginx/html_back  && git clone https://git.dev.tencent.com/zhishengqianjun/blog.git  /usr/share/nginx/html_back
 
 rm -rf /usr/share/nginx/html && mv /usr/share/nginx/html_back /usr/share/nginx/html
+
+sed -i "s/aisky.men/apesite.xyz/g" /usr/share/nginx/html/sitemap.xml
